4a22f3692835b25f69b84687e6b1e9faf481d1c2
K4R4 <mabdman512@gmail.com>
2023-11-07 01:21:55 +0900

    Add constants

---
+++ include/constant.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 20:37:11 by tkuramot          #+#    #+#             */
+/*   Updated: 2023/11/02 20:39:22 by tkuramot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONSTANT_H
+# define CONSTANT_H
+
+# define WINDOW_WIDTH 640
+# define WINDOW_HEIGHT 360
+
+typedef enum e_direction
+{
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST,
+	INIT
+}		t_direction;
+
+#endif
