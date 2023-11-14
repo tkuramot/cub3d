b518ae6dfbcb80d7cda7e98ba02e98a9b7e22c21
K4R4 <mabdman512@gmail.com>
2023-11-14 09:11:51 +0900

    たぶんできた

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 20:37:11 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/11/14 09:05:40 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/11/14 09:10:50 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,12 @@ int	game_loop(void *arg)
 		perform_dda(world, &dda);
 		dist_camera_plane_to_wall = get_dist_camera_plane_to_wall(&dda);
 		line_height = (int)(WINDOW_HEIGHT / dist_camera_plane_to_wall);
-		world->mlx_data.frame_buffer.img = mlx_new_image(world->mlx_data.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-		world->mlx_data.frame_buffer.addr = mlx_get_data_addr(world->mlx_data.frame_buffer.img, &world->mlx_data.frame_buffer.bits_per_pixel, &world->mlx_data.frame_buffer.line_length, &world->mlx_data.frame_buffer.endian);
+		// world->mlx_data.frame_buffer.img = mlx_new_image(world->mlx_data.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+		// world->mlx_data.frame_buffer.addr = mlx_get_data_addr(world->mlx_data.frame_buffer.img, &world->mlx_data.frame_buffer.bits_per_pixel, &world->mlx_data.frame_buffer.line_length, &world->mlx_data.frame_buffer.endian);
 		// draw_square(world, (t_vec2i){0, 0}, (t_vec2i){40, 40}, 0X00FF0000);
 		render_wall_vertical_line(world, x, line_height, 0X00FF0000);
-		mlx_put_image_to_window(world->mlx_data.mlx, world->mlx_data.mlx_win, world->mlx_data.frame_buffer.img, 0, 0);
 		x++;
 	}
+	mlx_put_image_to_window(world->mlx_data.mlx, world->mlx_data.mlx_win, world->mlx_data.frame_buffer.img, 0, 0);
 	return (0);
 }
