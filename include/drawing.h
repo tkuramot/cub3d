d81934579a2162f6d90d93a5933f894666a4eca4
K4R4 <mabdman512@gmail.com>
2023-11-14 10:27:56 +0900

    norm git add .

--- include/drawing.h
+++ include/drawing.h
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 20:37:11 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/11/14 10:24:46 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/11/14 10:27:48 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	init_mlx_data(t_mlx_data *mlx_data);
 void	apply_frame_buffer(t_mlx_data *mlx_data);
 void	allocate_frame_buffer(t_mlx_data *mlx_data);
 void	destroy_frame_buffer(t_mlx_data *mlx_data);
-void	draw_square(t_mlx_data *mlx_data, t_vec2i start, t_vec2i end, int color);
+void	draw_square(t_mlx_data *mlx_data,
+			t_vec2i start, t_vec2i end, int color);
 void	render_wall_vertical_line(t_mlx_data *mlx_data,
 			int x, int line_height, int color);
 
