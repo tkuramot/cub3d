8b57e2d5d3cdd8b3e778566bbc601d2b9860a096
Takafumi Okazaki <tokazaki@c1r27s11.42tokyo.jp>
2023-11-20 15:58:36 +0900

    fix move

--- src/game/player_movement.c
+++ src/game/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 01:12:48 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/11/19 18:51:08 by tokazaki         ###   ########.fr       */
+/*   Updated: 2023/11/20 15:58:14 by tokazaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@
 
 static void	move_player(t_world *world, double dy, double dx)
 {
-	double	old_y;
 	double	new_y;
 	double	new_x;
 
-	old_y = world->player.precise_pos.y;
 	new_y = world->player.precise_pos.y + dy * MOVEMENT_SPEED;
 	new_x = world->player.precise_pos.x + dx * MOVEMENT_SPEED;
 	if (world->map[(int)(new_y + dy / 100)][(int)world->player.precise_pos.x] \
 		!= WALL)
 		world->player.precise_pos.y = new_y;
-	if (world->map[(int)old_y][(int)(new_x + dx / 100)] != WALL)
+	if (world->map[(int)world->player.precise_pos.y][(int)(new_x + dx / 100)] \
+		!= WALL)
 		world->player.precise_pos.x = new_x;
 }
 
