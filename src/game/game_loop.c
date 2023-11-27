239648e9cdeb4e46d89856ff92e06113f9b8ae3c
Takafumi Okazaki <tokazaki@c1r12s3.42tokyo.jp>
2023-11-27 19:17:14 +0900

    dekita

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 20:37:11 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/11/26 11:57:49 by tokazaki         ###   ########.fr       */
+/*   Updated: 2023/11/27 19:16:27 by tokazaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@
 #include <stdio.h>
 #include <utils.h>
 #include <stdbool.h>
+
+#define MINIMAP_PLAYER_POS 110
+#define MINIMAP_RADIUS_SIZE 100
+#define MINIMAP_PIXEL_SIZE 10
+#define MINIMAP_MARGIN 3
+#define MINIMAP_WALL_BLOCK_SIZE 5
 
 void	move_forward(t_world *world);
 void	move_rightward(t_world *world);
@@ -58,35 +64,17 @@ void	key_handler(t_world *world)
 		move_rightward(world);
 }
 
-
+//色を透過して表示する方法
 void translucent_my_mlx_pixel_put(t_frame_buffer *frame_buffer,
                                    int x, int y, int color)
 {
     char *dst;
+    unsigned int *existing_color;
+    int alpha;
 
-    // 画像データの先頭アドレス + y行目 * 1行あたりのバイト数 + xピクセル目 * ピクセルあたりのバイト数
     dst = frame_buffer->addr + (y * frame_buffer->line_length + x * (frame_buffer->bits_per_pixel / 8));
-
-    // 既存の色
-    unsigned int *existing_color = (unsigned int *)dst;
-
-    // 新しい色を既存の色に対して重みづけして混ぜる
-    int alpha = 200;  // 重みづけの係数 (0から255の範囲で調整可能)
-//    int new_red = (color >> 16) & 0xFF;
-//    int new_green = (color >> 8) & 0xFF;
-//    int new_blue = color & 0xFF;
-//
-//    int existing_red = (*existing_color >> 16) & 0xFF;
-//    int existing_green = (*existing_color >> 8) & 0xFF;
-//    int existing_blue = (*existing_color) & 0xFF;
-//
-//    // 新しい色を既存の色に対して重みづけして混ぜる
-//    int blended_red = (existing_red * (255 - alpha) + new_red * alpha) / 255;
-//    int blended_green = (existing_green * (255 - alpha) + new_green * alpha) / 255;
-//    int blended_blue = (existing_blue * (255 - alpha) + new_blue * alpha) / 255;
-//    // ブレンディングされたRGB成分をセット
-//    *existing_color = (blended_red << 16) | (blended_green << 8) | blended_blue | (255 << 24);  // アルファは不透明として設定
-
+    existing_color = (unsigned int *)dst;
+    alpha = 200;
 	*existing_color = \
 		(((*existing_color & 0xFF00FF) * (255 - alpha) + (color & 0xFF00FF) * alpha) / 255) +
 		((((*existing_color >> 8) & 0xFF) * (255 - alpha) + ((color >> 8) & 0xFF) * alpha) / 255 << 8) +
@@ -94,6 +82,7 @@ void translucent_my_mlx_pixel_put(t_frame_buffer *frame_buffer,
 }
 
 #include <math.h>
+//円を表示する方法
 void draw_circle(t_world *world, int center, int radius, int color) {
 	int	angle;
 	int	x;
@@ -110,6 +99,33 @@ void draw_circle(t_world *world, int center, int radius, int color) {
 	}
 }
 
+//bool	is_render_mini_map(int x, int y)
+//{
+//	int	min_height;
+//	int	max_height;
+//
+//	min_height = MINIMAP_PLAYER_POS - MINIMAP_RADIUS_SIZE;
+//	max_height = MINIMAP_PLAYER_POS + MINIMAP_RADIUS_SIZE;
+//	if (x < min_height || max_height < x)
+//		return (false);
+//	if (y < min_height || max_height < y)
+//		return (false);
+//	return (true);
+//}
+
+bool	is_render_mini_map(int x, int y, int center, int radius)
+{
+	int	distance_squared;
+
+	distance_squared = (x - center) * (x - center) \
+					   + (y - center) * (y - center);
+	if (distance_squared <= radius * radius)
+		return (true);
+	else
+		return (false);
+}
+
+//minimapに壁ブロックの描画
 void	render_wall_brock(t_world *world, int i, int j)
 {
 	int k;
@@ -117,18 +133,18 @@ void	render_wall_brock(t_world *world, int i, int j)
 	int x;
 	int y;
 
-	x = (world->width - world->player.precise_pos.x) * 10;
-	y = (world->height - world->player.precise_pos.y) * 10;
+	x = MINIMAP_PLAYER_POS - world->player.precise_pos.x * MINIMAP_PIXEL_SIZE;
+	y = MINIMAP_PLAYER_POS - world->player.precise_pos.y * MINIMAP_PIXEL_SIZE;
 
-	k = 12;
-	while (k < 17)
+	k = 0;
+	while (k < 5)
 	{
-		l = 12;
-		while (l < 17)
+		l = 0;
+		while (l < 5)
 		{
-			if (0 < i + k + x - 100 && 0 < j + l + y -100)
+			if (is_render_mini_map(i + x + k, j + y + l, MINIMAP_PLAYER_POS, MINIMAP_RADIUS_SIZE) == true)
 			{
-				my_mlx_pixel_put(&world->mlx_data.frame_buffer, i + k + x - 100, j + l + y - 100, 0xFFFFFF);
+				my_mlx_pixel_put(&world->mlx_data.frame_buffer, i + k + x, j + l + y, 0xFFFFFF);
 			}
 			l++;
 		}
@@ -136,6 +152,7 @@ void	render_wall_brock(t_world *world, int i, int j)
 	}
 }
 
+//どのに壁があるか確認する
 void	render_map(t_world *world)
 {
 	int	i;
@@ -155,82 +172,50 @@ void	render_map(t_world *world)
 	}
 }
 
-//void translucent_my_mlx_pixel_put(t_frame_buffer *frame_buffer,
-//                                   int x, int y, int color)
-//{
-//    char *dst;
-//
-//    // 画像データの先頭アドレス + y行目 * 1行あたりのバイト数 + xピクセル目 * ピクセルあたりのバイト数
-//    dst = frame_buffer->addr + (y * frame_buffer->line_length + x * (frame_buffer->bits_per_pixel / 8));
-//
-//    // 既存の色
-//    unsigned int *existing_color = (unsigned int *)dst;
-//
-//    // 新しい色を既存の色と単純に混ぜ合わせる
-//    *existing_color = ((*existing_color + color) / 2);
-//}
+//#define MINIMAP_PLAYER_POS 100
+//#define MINIMAP_RADIUS_SIZE 100
+//#define MINIMAP_PIXEL_SIZE 10
+//#define MINIMAP_WALL_BLOCK_SIZE 5
 
 
+//mapのベースを描画する
 void	render_map_base(t_world *world)
 {
 	int	i;
 	int	j;
-	int x;
-	int y;
 
-	x = (world->width - world->player.precise_pos.x) * 10;
-	y = (world->height - world->player.precise_pos.y) * 10;
-
-	i = 5;
-	while (i < world->width * 10 + 15)
+	i = MINIMAP_PLAYER_POS - MINIMAP_RADIUS_SIZE - MINIMAP_MARGIN;
+	while (i < MINIMAP_PLAYER_POS + MINIMAP_RADIUS_SIZE + MINIMAP_MARGIN)
 	{
-		j = 5;
-		while (j < world->height * 10 + 15)
+		j = MINIMAP_PLAYER_POS - MINIMAP_RADIUS_SIZE - MINIMAP_MARGIN;
+		while (j < MINIMAP_PLAYER_POS + MINIMAP_RADIUS_SIZE + MINIMAP_MARGIN)
 		{
-			if (0 < i + x - 100 && 0 < j +  y -100)
-			{
-				translucent_my_mlx_pixel_put(&world->mlx_data.frame_buffer, i + x - 100, j + y - 100, 0xfff462);
-			}
+			if (is_render_mini_map(i, j, MINIMAP_PLAYER_POS, MINIMAP_RADIUS_SIZE) == true)
+				translucent_my_mlx_pixel_put(&world->mlx_data.frame_buffer, i, j, 0xfff462);
+			else if (is_render_mini_map(i, j, MINIMAP_PLAYER_POS, MINIMAP_RADIUS_SIZE + MINIMAP_MARGIN) == true)
+				my_mlx_pixel_put(&world->mlx_data.frame_buffer, i, j, 0xFFFFFF);
 			j++;
 		}
 		i++;
 	}
 }
 
-//void	render_player(t_world *world)
-//{
-//	int i;
-//	int j;
-//	int x;
-//	int y;
-//
-//	x = world->player.precise_pos.x * 10;
-//	y = world->player.precise_pos.y * 10;
-//	i = 8;
-//	while (i < 12)
-//	{	
-//		j = 8;
-//		while (j < 12)
-//		{
-//			my_mlx_pixel_put(&world->mlx_data.frame_buffer, x + i, y + j, 0x0);
-//			j++;
-//		}
-//		i++;
-//	}
-//}
-
+//プレイヤーの場所の表示
 void	render_player(t_world *world)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	while (i < 5)
+	i = -6;
+	while (i < -1)
 	{
-		j = 0;
-		while (j < 5)
+		j = -5;
+		while (j < 0)
 		{
-			my_mlx_pixel_put(&world->mlx_data.frame_buffer, world->width * 10 - 2 + i + 10 - 100, world->height * 10 - 2 + j + 10 - 100, 0x0);
+			if (0 < i + MINIMAP_PLAYER_POS && 0 < j + MINIMAP_PLAYER_POS)
+			{
+				my_mlx_pixel_put(&world->mlx_data.frame_buffer, i + MINIMAP_PLAYER_POS, j + MINIMAP_PLAYER_POS, 0x0);
+			}
 			j++;
 		}
 		i++;
@@ -260,10 +245,6 @@ int	game_loop(t_world *world)
 	}
 	render_map_base(world);
 	render_map(world);
-//    draw_circle(world, 100, 90, 0xFFFFFF);
-//    draw_circle(world, 100, 91, 0xFFFFFF);
-//    draw_circle(world, 100, 92, 0xFFFFFF);
-//    draw_circle(world, 100, 95, 0xFFFFFF);
 	render_player(world);
 
 	frame_buffer_apply(&world->mlx_data);
